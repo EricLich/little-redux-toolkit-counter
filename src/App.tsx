df460b163055debc0b7d6302505c18ea451502f7
Eric Lich <eric.lichtensztein@aleia.io>
2022-12-29 00:09:06 -0300

    add main store and counter slice

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,13 @@ function App() {
     <div className="App">
       <h1>Vite + React + Redux toolkit + TS</h1>
       <div className="card">
+        <h3>Counter with useState</h3>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <h3>Counter with Redux toolkit</h3>
+      <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
