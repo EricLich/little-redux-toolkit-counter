f177d24ed2158b55925cf0f5b53941c3b01eae12
Eric Lich <eric.lichtensztein@aleia.io>
2022-12-29 00:24:13 -0300

    add the new counter to ui with all reducer functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./store";
+import { decrement, increment, incrementByAmount } from "./slices/counterSlice";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [increaseBy, setIncreaseBy] = useState<number>(0);
+  const countRedux = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncreaseBy(): void {
+    dispatch(incrementByAmount(increaseBy));
+    setIncreaseBy((prev) => (prev = 0));
+  }
 
   return (
     <div className="App">
@@ -14,11 +25,19 @@ function App() {
           count is {count}
         </button>
       </div>
-      <h3>Counter with Redux toolkit</h3>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h3>Counter with useState</h3>
+        <input
+          type="number"
+          value={increaseBy}
+          onChange={(e) =>
+            setIncreaseBy((prev) => (prev = parseInt(e.target.value)))
+          }
+        />
+        <button onClick={handleIncreaseBy}>Increase by amount</button>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <p>{countRedux}</p>
       </div>
     </div>
   );
